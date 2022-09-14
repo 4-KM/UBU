2bb32c78a655cdb50afa9969df56855a01ca5947
Gersai <gersai_ventura@hotmail.com>
2022-09-13 23:49:56 -0500

    Initial Commit

---
+++ UBU/UBUApp.swift
@@ -0,0 +1,17 @@
+//
+//  UBUApp.swift
+//  UBU
+//
+//  Created by eduardo gersai  on 13/09/22.
+//
+
+import SwiftUI
+
+@main
+struct UBUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
